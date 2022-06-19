8c0b4ffdbc2c4d2420f949f774588ffd77e542e6
ShinHeeSeung <tlsgmltmd726529@gmail.com>
2022-06-20 00:17:18 +0900

    add reset

--- word_collect.py
+++ word_collect.py
@@ -25,20 +25,21 @@ def open_db():
     )
 
     cursor = conn.cursor()
+    # cursor.execute("DROP TABLE IF EXISTS wordSiyeon")
+    # # # 테이블 생성하기
+    # cursor.execute("CREATE TABLE wordSiyeon (NO int, title text, url text, album text)")
+    # conn.commit()
+
+def reset():
+    # # 실행할 때마다 다른값이 나오지 않게 테이블을 제거해두기
+    open_db()
     cursor.execute("DROP TABLE IF EXISTS wordSiyeon")
-    # # 테이블 생성하기
+    # 테이블 생성하기
     cursor.execute("CREATE TABLE wordSiyeon (NO int, title text, url text, album text)")
     conn.commit()
 
 
-# # 실행할 때마다 다른값이 나오지 않게 테이블을 제거해두기
-# cursor.execute("DROP TABLE IF EXISTS melon")
-# # 테이블 생성하기
-# cursor.execute("CREATE TABLE melon (NO int, title text, url text, album text)")
-# conn.commit()
-
-
-def search(url):
+def search(url, count):
     r = session.get(url)
     html = r.text
     soup = BeautifulSoup(html, 'html.parser')
@@ -47,17 +48,17 @@ def search(url):
     for a in links:
         href = str(a.attrs['href'])
         text = a.string
+        if count != 0 and no >= count:
+            break
         if "url=" in href:  # ahmia data parsing
             index = href.find("url=") + 4
             href = href[index:]
         if "http://" in href:
             try:
-                print("---------------------------------------------------------------------------------------")
                 request = session.get(href)
                 html = request.text
                 soup = BeautifulSoup(html, 'html.parser')
                 text = text.rstrip()
-                print(text)
                 data = soup.get_text().rstrip()
                 if text != "Last Week" and text != "Last Day" and text != "Last Month" and text != "Any Time":
                     if len(data) < 65535 and len(text) < 65535:
@@ -65,36 +66,43 @@ def search(url):
                         cursor.execute(sql1, (text, data))
                         result = cursor.fetchall()
                         if len(result) == 0:
+                            print("---------------------------------------------------------------------------------------")
                             print(no + 1, "번째 데이터가 수집 되었습니다.")
                             sql = "INSERT INTO wordSiyeon (no, title, url, album) VALUES (%s, %s, %s, %s)"
                             cursor.execute(sql, (no, text, href, data))
                             no += 1
                             conn.commit()
                     else:
-                        print("maxlen")
+                        print("---------------------------------------------------------------------------------------")
+                        print("데이터 크기로 인한 수집 불가")
+                        print("---------------------------------------------------------------------------------------")
             except requests.exceptions.RequestException as erra:
-                print("AnyException : ", erra)
+                print("---------------------------------------------------------------------------------------")
+                print("잘못된 웹 페이지입니다.")
+                print("---------------------------------------------------------------------------------------")
 
 
-def ahmia(keyword):
+def ahmia(keyword, count):
     url = 'http://juhanurmihxlp77nkq76byazcldy2hlmovfu2epvl5ankdibsot4csyd.onion/search/?q='
     for word in keyword:
         url_ah = url + word
-        print(word)
-        search(url_ah)
+        print("====================================== ahmia ==========================================")
+        print(word + "와 관련된 문자 데이터 수집 시작합니다.")
+        search(url_ah, count)
 
 
-def torsearch(keyword):
+def torsearch(keyword, count):
     url = 'http://torchdeedp3i2jigzjdmfpn5ttjhthh5wbmda2rr3jvqjg5p77c54dqd.onion/search?query='
     for word in keyword:
         url_tor = url + word
-        print(word)
-        search(url_tor)
+        print("===================================== torsearch =========================================")
+        print(word + "와 관련된 문자 데이터 수집 시작합니다.")
+        search(url_tor, count)
 
 
-def start(query):
+def start(query, count):
     socket()
     open_db()
-    ahmia(query)
-    # torsearch(query)
+    ahmia(query, count)
+    torsearch(query, count)
     conn.close()
